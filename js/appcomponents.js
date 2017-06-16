c165304c9034103a4187bbb28c32fe81a3769741
Benjamin Cabé <benjamin@eclipse.org>
2017-06-16 18:10:11 +0200

    Update 2017-06-16T16:10:09.857Z

--- js/appcomponents.js
+++ js/appcomponents.js
@@ -1,1 +1,383 @@
-!function(){function o(o,e,t,r){e.html5Mode(!1),o.when("/",{templateUrl:"views/home.html",controller:"MainController",controllerAs:"main"}).when("/contact",{templateUrl:"views/contact.html",controller:"MainController",controllerAs:"main"}).when("/setup",{templateUrl:"views/setup.html",controller:"MainController",controllerAs:"main"}).otherwise({redirectTo:"/"}),t.interceptors.push("authInterceptor")}function e(o,e,t,r){return{request:function(o){return o.headers=o.headers||{},o},responseError:function(o){return 404===o.status?(r.path("/"),e.reject(o)):e.reject(o)}}}function t(o,e){}angular.module("boilerplate",["ngRoute","ngMap","ngMQTT"]).config(o).config(["MQTTProvider",function(o){o.setHref("ws://iot.eclipse.org:80/ws")}]),o.$inject=["$routeProvider","$locationProvider","$httpProvider","$compileProvider"],angular.module("boilerplate").factory("authInterceptor",e),e.$inject=["$rootScope","$q","LocalStorage","$location"],angular.module("boilerplate").run(t),t.$inject=["$rootScope","$location"]}(),function(){angular.module("boilerplate").constant("CONSTANTS",{API_URL:"http://www.yourAPIurl.com/"})}(),function(){"use strict";function o(o,e){function t(e,t){return i||console.log("localStorage not supported, make sure you have the $cookies supported."),null===window.localStorage.getItem(e)?o.localStorage&&o.localStorage.setItem(e,angular.toJson(t)):void console.warn("localStorage with the name "+e+" already exists. Please pick another name.")}function r(e){return i||console.log("localStorage not supported, make sure you have the $cookies supported."),o.localStorage&&angular.fromJson(o.localStorage.getItem(e))}function n(e,t){return i||console.log("localStorage not supported, make sure you have the $cookies supported."),o.localStorage&&o.localStorage.setItem(e,angular.toJson(t))}function l(e){return i||console.log("localStorage not supported, make sure you have the $cookies supported."),o.localStorage&&o.localStorage.removeItem(e)}function a(){return i||console.log("localStorage not supported, make sure you have the $cookies supported."),o.localStorage&&o.localStorage.clear()}function c(){return o.localStorage}var u="undefined"==typeof window.localStorage?void 0:window.localStorage,i=!(void 0===typeof u||void 0===typeof window.JSON);return angular.element(o).on("storage",function(o,t){o.key===t&&e.$apply()}),{set:t,get:r,update:n,remove:l,removeAll:a,list:c}}angular.module("boilerplate").factory("LocalStorage",["$window","$rootScope",o])}(),function(){"use strict";function o(o,e,t){function r(r,n,l,a){var c=e.defer();return o({method:r,url:t.API_URL+n,params:l,data:a}).then(function(o){o.config||console.log("Server error occured."),c.resolve(o)},function(o){c.reject(o)}),c.promise}var n={query:r};return n}angular.module("boilerplate").factory("QueryService",["$http","$q","CONSTANTS",o])}();
+/**
+ * 
+ * AngularJS Boilerplate
+ * @description           Description
+ * @author                Jozef Butko // www.jozefbutko.com/resume
+ * @url                   www.jozefbutko.com
+ * @version               1.1.7
+ * @date                  March 2015
+ * @license               MIT
+ * 
+ */
+;(function() {
+
+
+  /**
+   * Definition of the main app module and its dependencies
+   */
+  angular
+    .module('boilerplate', [
+      'ngRoute', 'ngMap', 'ngMQTT'
+    ])
+    .config(config)
+    .config(['MQTTProvider',function(MQTTProvider){
+        MQTTProvider.setHref('ws://iot.eclipse.org:80/ws');
+    }]);
+
+  // safe dependency injection
+  // this prevents minification issues
+  config.$inject = ['$routeProvider', '$locationProvider', '$httpProvider', '$compileProvider'];
+
+  /**
+   * App routing
+   *
+   * You can leave it here in the config section or take it out
+   * into separate file
+   * 
+   */
+  function config($routeProvider, $locationProvider, $httpProvider, $compileProvider) {
+
+    $locationProvider.html5Mode(false);
+
+    // routes
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .when('/setup', {
+        templateUrl: 'views/setup.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+    $httpProvider.interceptors.push('authInterceptor');
+
+  }
+
+
+  /**
+   * You can intercept any request or response inside authInterceptor
+   * or handle what should happend on 40x, 50x errors
+   * 
+   */
+  angular
+    .module('boilerplate')
+    .factory('authInterceptor', authInterceptor);
+
+  authInterceptor.$inject = ['$rootScope', '$q', 'LocalStorage', '$location'];
+
+  function authInterceptor($rootScope, $q, LocalStorage, $location) {
+
+    return {
+
+      // intercept every request
+      request: function(config) {
+        config.headers = config.headers || {};
+        return config;
+      },
+
+      // Catch 404 errors
+      responseError: function(response) {
+        if (response.status === 404) {
+          $location.path('/');
+          return $q.reject(response);
+        } else {
+          return $q.reject(response);
+        }
+      }
+    };
+  }
+
+
+  /**
+   * Run block
+   */
+  angular
+    .module('boilerplate')
+    .run(run);
+
+  run.$inject = ['$rootScope', '$location'];
+
+  function run($rootScope, $location) {
+
+    // put here everything that you need to run on page load
+
+  }
+
+
+})();
+;(function() {
+
+
+	/**
+	 * Place to store API URL or any other constants
+	 * Usage:
+	 *
+	 * Inject CONSTANTS service as a dependency and then use like this:
+	 * CONSTANTS.API_URL
+	 */
+  angular
+  	.module('boilerplate')
+    .constant('CONSTANTS', {
+      'API_URL': 'http://www.yourAPIurl.com/'
+    });
+
+
+})();
+
+;(function() {
+
+
+    'use strict';
+
+
+    /**
+     * Service for complex localStorage functionality
+     *
+     * @category  factory
+     * @author    Jozef Butko
+     * @example   Inject LocalStorage as the dependency and then use it like this:
+     *
+     * var data = { property: 'name'};
+     * // set, get, remove, removeAll and list localStorage values
+     * LocalStorage.set('obj', data);
+     * LocalStorage.get('obj');
+     * LocalStorage.update('obj', data);
+     * LocalStorage.remove('obj');
+     * LocalStorage.removeAll();
+     * LocalStorage.list();
+     *
+     * @version   1.0
+     *
+     */
+    angular
+      .module('boilerplate')
+      .factory('LocalStorage', [
+        '$window', '$rootScope', LocalStorageService
+      ]);
+
+
+    //////////////// factory
+
+
+    function LocalStorageService($window, $rootScope) {
+
+      /**
+       * Test browser if it supports localStorage
+       */
+      var storage = (typeof window.localStorage === 'undefined') ? undefined : window.localStorage,
+          supported = !(typeof storage === undefined || typeof window.JSON === undefined);
+
+        /*
+        * whenever localStorage gets updated trigger
+        * $digest cicle so all values are refreshed in the view
+         */
+        angular.element($window).on('storage', function(event, name) {
+          if (event.key === name) {
+            $rootScope.$apply();
+          }
+        });
+
+
+        return {
+          set: set,
+          get: get,
+          update: update,
+          remove: remove,
+          removeAll: removeAll,
+          list: list
+        };
+
+
+        //////////////// function definitions
+
+
+        /**
+         * Set localStorage value and check if it already do not exists
+         *
+         * @param {string} name Name of localStorage value
+         * @param {object} val  Return stored value
+         */
+         function set(name, val) {
+           if (!supported) {
+               console.log('localStorage not supported, make sure you have the $cookies supported.');
+             }
+
+           // in case we already have localStorage with same name alert error msg
+           if (window.localStorage.getItem(name) !== null) {
+             console.warn('localStorage with the name ' + name + ' already exists. Please pick another name.');
+           } else {
+             return $window.localStorage && $window.localStorage.setItem(name, angular.toJson(val));
+           }
+         }
+
+
+         /**
+          * getData from localStorage
+          *
+          * @param  {string} name Name of localStorage value
+          * @return {*}           Stored value
+          */
+         function get(name) {
+           if (!supported) {
+               console.log('localStorage not supported, make sure you have the $cookies supported.');
+             }
+
+           return $window.localStorage && angular.fromJson($window.localStorage.getItem(name));
+         }
+
+
+         /**
+          * Update already stored data
+          *
+          * @param  {string}  name Name of localStorage value
+          * @param {object}   val  Return stored value
+          */
+         function update(name, val) {
+           if (!supported) {
+               console.log('localStorage not supported, make sure you have the $cookies supported.');
+             }
+
+           return $window.localStorage && $window.localStorage.setItem(name, angular.toJson(val));
+         }
+
+
+
+         /**
+          * Remove localStorage value
+          *
+          * @param  {string} name Name of localStorage value
+          * @return {boolean}     True/false if the value is removed
+          */
+         function remove(name) {
+           if (!supported) {
+               console.log('localStorage not supported, make sure you have the $cookies supported.');
+           }
+
+           return $window.localStorage && $window.localStorage.removeItem(name);
+         }
+
+
+         /**
+          * Remove all localStorage values
+          *
+          * @return {boolean}     True/false if the value is removed
+          */
+         function removeAll() {
+           if (!supported) {
+               console.log('localStorage not supported, make sure you have the $cookies supported.');
+           }
+
+           return $window.localStorage && $window.localStorage.clear();
+         }
+
+
+         /**
+          * Return object of all values that are stored on localStorage
+          *
+          * @return {object}    Object with all data stored on localStorage
+          */
+         function list() {
+           return $window.localStorage;
+         }
+
+    }
+
+
+})();
+
+;(function() {
+
+
+  'use strict';
+
+
+  /**
+   * $http service abstraction to make API calls with any HTTP method,
+   * custom url and data object to be sent as request.
+   * Every REST API call is measured, you can see how much it took
+   * in the console.
+   *
+   * @category  factory
+   * @author    Jozef Butko
+   * @example   Inject QueryService as the dependency and then use it this way:
+   *
+   * QueryService.query('GET', 'users/user/', {get: query}, {post: params})
+      .then(function(data) {
+        console.log(data);
+      }, function(error) {
+        console.log(error);
+      });
+   *
+   * @param     {String} method HTTP method, eg. 'PUT', 'GET'...
+   * @param     {String} url API endpoint, eg. 'users/user' or 'system-properties'
+   * @param     {Object} params Map of strings or objects which will be turned
+   *                     to `?key1=value1&key2=value2` after the url. If the value
+   *                     is not a string, it will be
+   *                     JSONified
+   * @return    {Object} data Data to be sent as the request message data
+   * @version   1.1
+   *
+   */
+
+
+  angular
+    .module('boilerplate')
+    .factory('QueryService', [
+      '$http', '$q', 'CONSTANTS', QueryService
+    ]);
+
+
+
+  //////////////// factory
+
+
+
+  function QueryService($http, $q, CONSTANTS) {
+
+
+    var service = {
+      query: query
+    };
+
+    return service;
+
+
+    //////////////// definition
+
+
+    function query(method, url, params, data) {
+
+      var deferred = $q.defer();
+
+      $http({
+        method: method,
+        url: CONSTANTS.API_URL + url,
+        params: params,
+        data: data
+      }).then(function(data) {
+        if (!data.config) {
+          console.log('Server error occured.');
+        }
+        deferred.resolve(data);
+      }, function(error) {
+        deferred.reject(error);
+      });
+
+      return deferred.promise;
+    }
+
+  }
+
+
+})();
--- js/mainapp.js
+++ js/mainapp.js
@@ -1,1 +1,113 @@
-!function(){function o(o,t){return{loadData:loadData}}angular.module("boilerplate").factory("getDataFromAPI",o),o.$inject=["$http","LocalStorage"]}(),function(){function o(o,t,n,a){var l=this;l.positions=[],t.on("djidrone",function(o){console.log(o),l.positions=[];var t=o.latitude||43.551977,n=o.longitude||1.470873;l.positions.push({lat:t,lng:n,data:o})})}angular.module("boilerplate").controller("MainController",o)}();
+;(function() {
+
+
+  /**
+   * Sample factory
+   *
+   * You can fetch here some data from API and the use them
+   * in controller
+   * 
+   */
+  angular
+    .module('boilerplate')
+    .factory('getDataFromAPI', getDataFromAPI);
+
+  getDataFromAPI.$inject = ['$http', 'LocalStorage'];
+
+
+  ////////////
+
+
+  function getDataFromAPI($http, LocalStorage) {
+
+    return {
+      loadData: loadData
+    };
+
+
+    ////////////  function definitions
+
+
+    /**
+     * Load articles from GetPocket API
+     * @return {Object} Articles object
+     */
+    // var request = {
+    //   consumer_key: 'xxxx',
+    //   access_token: 'xxxx',
+    //   sort: 'newest',
+    //   count: 5
+    // };
+
+    // return $http({
+    //   method: 'GET',
+    //   url: API.url + 'v3/get',
+    //   params: request
+    // }).then(function(articles) {
+    //   return articles.data;
+    // })
+    // .catch(function(error) {
+    //   return error;
+    // });
+  }
+
+
+})();
+
+/**
+ * Main application controller
+ *
+ * You can use this controller for your whole app if it is small
+ * or you can have separate controllers for each logical section
+ * 
+ */
+;(function() {
+
+  angular
+    .module('boilerplate')
+    .controller('MainController', MainController);
+
+//  MainController.$inject = ['LocalStorage', 'QueryService'];
+
+
+  function MainController(NgMap, MQTTService, $interval, $scope) {
+    var vm = this;
+    vm.positions = [];
+
+     MQTTService.on('djidrone', function(data){
+            console.log(data);
+            vm.positions = [];
+            var lat = data.latitude || 43.551977;
+            var lng = data.longitude || 1.470873;
+            vm.positions.push({lat:lat, lng:lng, data:data});
+          // $scope.$apply();
+        });
+
+            // vm.positions = [];
+            // var lat = 43.551855;
+            // var lng =  1.471081;
+            // var data = {} ;
+            // data.pitch = 0;
+            // data.yaw = 0;
+            // data.roll = 0;
+            // vm.positions.push({lat:lat, lng:lng, data:data});
+
+
+    // var generateMarkers = function() {
+    //   vm.positions = [];
+    //   var numMarkers = Math.floor(Math.random() * 4) + 4; //between 4 to 8 markers
+    //   for (i = 0; i < numMarkers; i++) {
+    //     var lat = 43.6600000 + (Math.random() / 100);
+    //     var lng = -79.4103000 + (Math.random() / 100);
+    //     vm.positions.push({lat:lat, lng:lng});
+    //   }
+    //   //console.log("vm.positions", vm.positions);
+    // };
+
+    // $interval(generateMarkers, 100);
+
+
+  }
+
+
+})();
